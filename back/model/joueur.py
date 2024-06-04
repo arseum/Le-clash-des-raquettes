eff2dcffbec57f5312f81b9c423c908c9a744053
amaitre <manbrozer@gmail.com>
2024-06-04 16:38:34 +0200

    wip model

--- back/model/joueur.py
+++ back/model/joueur.py
@@ -1,3 +1,9 @@
 class Joueur:
+    id: int
+    nom: str
+    prenom: str
+    date_naissance: any
+    niveau: any
+
     def __init__(self):
         pass
--- back/model/match.py
+++ back/model/match.py
@@ -1,3 +1,16 @@
+from back.model.joueur import Joueur
+from back.model.tournoi import Tournoi
+
+
 class Match:
+    id: int
+    joueur_1: Joueur
+    joueur_2: Joueur
+    tournoi: Tournoi
+    table: int
+    score: (int, int)
+    heure: any
+    phase: any
+
     def __init__(self):
         pass
