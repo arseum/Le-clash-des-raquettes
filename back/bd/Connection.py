2a849587cade348c12929fc9875b6db2e0fe7ffe
amaitre <manbrozer@gmail.com>
2024-06-10 14:25:15 +0200

    format v1

--- back/bd/Connection.py
+++ back/bd/Connection.py
@@ -1,14 +1,14 @@
 from pymongo import MongoClient
 
 # URL de connexion MongoDB Atlas
-uri = "mongodb+srv://faelby:<2NUEp2jDCcd3kod7>@cluster0.5bsuhkg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = ("mongodb+srv://faelby:<EpO1QKzA6pA23HqV>@cluster0.5bsuhkg.mongodb.net/?directConnection=true&retryWrites=true&w"
+       "=majority&appName=Cluster0")
+
 
 def connect_to_mongodb():
     try:
-        # Connexion à MongoDB
         client = MongoClient(uri)
 
-        # Accès à la base de données (remplacez 'votre_base_de_donnees' par le nom réel de votre base de données)
         db = client.ClashDesRaquettes
 
         # Test de la connexion en listant les collections
@@ -24,6 +24,6 @@ def connect_to_mongodb():
         print("Échec de la connexion à MongoDB Atlas : ", e)
         return None
 
+
 if __name__ == "__main__":
-    # Connexion à MongoDB et récupération de la base de données
     db = connect_to_mongodb()
--- back/model/tournoi.py
+++ back/model/tournoi.py
@@ -15,3 +15,42 @@ class Tournoi:
 
     def __init__(self):
         pass
+
+    def calcule_format(self):
+        nb_participants = len(self.joueurs)
+        for i in range(2, 10):
+            nb_joueurs_brackets = 2 ** i
+            if nb_participants == nb_joueurs_brackets:
+                return f"braquet direct de {nb_joueurs_brackets}"
+            if nb_participants > nb_joueurs_brackets:
+                for j in range(2, i // 2, 2):
+                    for k in range(1, 10):
+                        if (j + k) * (i // j) == nb_participants:
+                            print(f"{j} poule de {i // j}")
+
+
+def format(nb_participants, nb_table):
+    if nb_participants <= 6:
+        print("a la ronde")
+
+    nb_joueurs_brackets = 4
+    if nb_participants == nb_joueurs_brackets:
+        print(f"braquet direct de {nb_joueurs_brackets}")
+    if nb_participants > nb_joueurs_brackets:
+        for nb_poule in [2, 4]:
+            if nb_poule == 2:
+                poules = [nb_participants // nb_poule + (j and nb_participants % 2) for j in range(2)]
+            else:
+                poules = [nb_participants // nb_poule + j for j in
+                          [nb_participants / nb_poule % 1 >= 0.25 * k for k in range(1, 5)]]
+            temp_tournoi = 14
+            for p in poules:
+                temp_tournoi += p*(p-1)/2/nb_table * 8
+            print(f"{nb_poule} poules: {poules} pour une durée de {temp_tournoi}min")
+
+
+if __name__ == "__main__":
+    for i in range(20):
+        print()
+        print(f"format de tournoi {4 + i} participants")
+        format(4 + i, 2)
