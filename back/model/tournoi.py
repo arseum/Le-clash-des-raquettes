b72667842898165daa5ae6262403ca86447bf433
amaitre <manbrozer@gmail.com>
2024-06-10 14:52:20 +0200

    format v2

--- back/model/tournoi.py
+++ back/model/tournoi.py
@@ -1,51 +1,27 @@
-from back.model.joueur import Joueur
-from back.model.match import Match
+def calcul_durre_poule(taille_poule, nb_table):
+    return taille_poule * (taille_poule - 1) / 2 / nb_table * 8
 
 
-class Tournoi:
-    id: int
-    nom: str
-    lieu: str
-    joueurs: [Joueur]
-    matchs: [Match]
-    date_debut: any
-    date_fin: any
-    niveau: any
-    categorie: any
-
-    def __init__(self):
-        pass
-
-    def calcule_format(self):
-        nb_participants = len(self.joueurs)
-        for i in range(2, 10):
-            nb_joueurs_brackets = 2 ** i
-            if nb_participants == nb_joueurs_brackets:
-                return f"braquet direct de {nb_joueurs_brackets}"
-            if nb_participants > nb_joueurs_brackets:
-                for j in range(2, i // 2, 2):
-                    for k in range(1, 10):
-                        if (j + k) * (i // j) == nb_participants:
-                            print(f"{j} poule de {i // j}")
+def determine_poule(nb_participants, nb_poule):
+    return [nb_participants // nb_poule + (j and nb_participants % 2) for j in range(2)] if nb_poule == 2 else [
+        nb_participants // nb_poule + j for j in [nb_participants / nb_poule % 1 >= 0.25 * k for k in range(1, 5)]]
 
 
-def format(nb_participants, nb_table):
-    if nb_participants <= 6:
-        print("a la ronde")
+def format(nb_participants, nb_table, temps_max):
+    durre_championat = calcul_durre_poule(nb_participants, nb_table)
+    if nb_participants <= 8 and durre_championat <= temps_max:
+        print(f"a la ronde pour une durée de {durre_championat}min")
 
     nb_joueurs_brackets = 4
-    if nb_participants == nb_joueurs_brackets:
-        print(f"braquet direct de {nb_joueurs_brackets}")
-    if nb_participants > nb_joueurs_brackets:
-        for nb_poule in [2, 4]:
-            if nb_poule == 2:
-                poules = [nb_participants // nb_poule + (j and nb_participants % 2) for j in range(2)]
-            else:
-                poules = [nb_participants // nb_poule + j for j in
-                          [nb_participants / nb_poule % 1 >= 0.25 * k for k in range(1, 5)]]
-            temp_tournoi = 14
-            for p in poules:
-                temp_tournoi += p*(p-1)/2/nb_table * 8
+    if nb_participants <= nb_joueurs_brackets:
+        return "au revoir"
+
+    for nb_poule in [2, 4]:
+        poules = determine_poule(nb_participants, nb_poule)
+        if 2 in poules:
+            continue
+        temp_tournoi = 14 + sum(calcul_durre_poule(p, nb_table) for p in poules)
+        if temp_tournoi <= temps_max:
             print(f"{nb_poule} poules: {poules} pour une durée de {temp_tournoi}min")
 
 
@@ -53,4 +29,4 @@ if __name__ == "__main__":
     for i in range(20):
         print()
         print(f"format de tournoi {4 + i} participants")
-        format(4 + i, 2)
+        format(4 + i, 2, 120)
